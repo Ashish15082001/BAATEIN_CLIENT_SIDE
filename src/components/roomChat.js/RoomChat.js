ce685ba49aff39b8f3909a34507e517573bc7834
Ashish Singh <ashusingh15082001@gmail.com>
2021-11-23 14:31:17 +0530

    version 23.11.2021

--- src/components/roomChat.js/RoomChat.js
+++ src/components/roomChat.js/RoomChat.js
@@ -1,9 +1,10 @@
 import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { v4 } from "uuid";
 import { socket } from "../..";
 import classes from "./RoomChat.module.css";
+import ReactScollableFeed from "react-scrollable-feed";
 
 export default function RoomChat(props) {
   const { userName } = props;
@@ -11,7 +12,6 @@ export default function RoomChat(props) {
   const { roomId, members } = joinedRoomData;
   const [messages, setMessages] = useState(props.joinedRoomData.messages);
   const [typedMessage, setTypedMessage] = useState("");
-  const chatListRef = useRef(null);
 
   const onTypedMessageChanged = function (event) {
     setTypedMessage(event.target.value);
@@ -35,7 +35,6 @@ export default function RoomChat(props) {
       return [...oldMessages, { message: typedMessage, sender: userName }];
     });
     setTypedMessage("");
-    chatListRef.current.scrollBy(0, 1000);
   };
 
   return (
@@ -43,42 +42,46 @@ export default function RoomChat(props) {
       <div className={classes.mainContainer}>
         <div className={classes.activeUsersContainer}>
           <h2>Active users</h2>
-          <ul className={classes.activeUsersList}>
-            {members.map((member) => (
-              <li key={v4()}>{member}</li>
-            ))}
-          </ul>
+          <ReactScollableFeed>
+            <ul className={classes.activeUsersList}>
+              {members.map((member) => (
+                <li key={v4()}>{member}</li>
+              ))}
+            </ul>
+          </ReactScollableFeed>
         </div>
         <div className={classes.chatContainer}>
           <div className={classes.actualContainer}>
-            <ul ref={chatListRef} className={classes.messageList}>
-              {messages.map((messageItem) => (
-                <li
-                  className={
-                    classes[
-                      userName === messageItem.sender
-                        ? "selfMessage"
-                        : "othersMessage"
-                    ]
-                  }
-                  key={v4()}
-                >
-                  <p
+            <ul className={classes.messageList}>
+              <ReactScollableFeed>
+                {messages.map((messageItem) => (
+                  <li
                     className={
                       classes[
                         userName === messageItem.sender
-                          ? "selfActualMessage"
-                          : "othesrActualMessage"
+                          ? "selfMessage"
+                          : "othersMessage"
                       ]
                     }
+                    key={v4()}
                   >
-                    {messageItem.message}
-                    <span className={classes.messageSenderName}>
-                      ~{messageItem.sender}
-                    </span>
-                  </p>
-                </li>
-              ))}
+                    <p
+                      className={
+                        classes[
+                          userName === messageItem.sender
+                            ? "selfActualMessage"
+                            : "othesrActualMessage"
+                        ]
+                      }
+                    >
+                      {messageItem.message}
+                      <span className={classes.messageSenderName}>
+                        ~{messageItem.sender}
+                      </span>
+                    </p>
+                  </li>
+                ))}
+              </ReactScollableFeed>
             </ul>
           </div>
           <div className={classes.chatContainerBottom}>
